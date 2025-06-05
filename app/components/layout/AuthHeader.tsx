ffe32777560c5d558b479ba9087981e63a708eae
adrian-sullca <adrian.infooo@gmail.com>
2025-06-05 20:55:50 +0200

    UI updated: Dropdown implemented in the auth header

--- app/components/layout/AuthHeader.tsx
+++ app/components/layout/AuthHeader.tsx
@@ -1,10 +1,23 @@
-import { Link, NavLink } from "@remix-run/react";
+import { Link, NavLink, useLoaderData } from "@remix-run/react";
 import { SecureVisitLogoBlue } from "~/components/utils/Icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { User as UserIcon } from "lucide-react";
+import { loader } from "~/routes/_auth";
 
 export default function AuthHeader() {
+  const { user } = useLoaderData<typeof loader>();
   return (
     <header className="fixed w-full h-14 border-b z-50 bg-white">
-      <div className="text-black h-full max-w-7xl flex justify-between mx-auto px-4">
+      <div className="container text-black h-full max-w-7xl flex justify-between mx-auto px-4 xl:px-0">
         <SecureVisitLogoBlue />
         <div className="flex items-center">
           <NavLink
@@ -27,7 +40,23 @@ export default function AuthHeader() {
           >
             Graficos
           </NavLink>
-          <Link to="/logout">Cerrar sesión</Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">
+                <>
+                  <UserIcon className="text-gray-600"></UserIcon>
+                  <span className="text-gray-700">{user.name}</span>
+                </>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" side="bottom" align="end">
+              <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link to="/logout">Cerrar sesión</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </header>
--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,12 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import Footer from "~/components/layout/Footer";
-import { Outlet } from "@remix-run/react";
+import { json, Outlet, useLoaderData } from "@remix-run/react";
 import AuthHeader from "~/components/layout/AuthHeader";
+import { getUserByToken, requireAuth } from "~/server/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const authToken = await requireAuth(request);
+  const user = await getUserByToken(request);
+  return json({
+    user: user, 
+  });
+};
 
 export default function Index() {
+  const { user } = useLoaderData<typeof loader>();
   return (
     <>
-      <AuthHeader />
+      <AuthHeader user={user} />
       <main className="pt-28 pb-14 px-4 min-h-screen bg-[#f7f9fb]">
         <Outlet />
       </main>
